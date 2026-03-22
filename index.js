9f8266f0bceeb9b08dec9c7ebf9e2e871b67c85a
XNAK3 <yagami01018@gmail.com>
2026-03-22 02:21:59 -0400

    Add some user interface

--- index.js
+++ index.js
@@ -1,80 +1,97 @@
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['👊', '✋', '✌️'];
+const playerChoice = document.getElementById('pChoice');
+const computerChoice = document.getElementById('cChoice');
+const result = document.getElementById('result');
+const pScore = document.getElementById('pScore');
+const cScore = document.getElementById('cScore');
+const ties = document.getElementById('ties');
+const roundNum = document.getElementById('roundNum');
+const winner = document.getElementById('winner');
+
 let humanScore = 0;
 let computerScore = 0;
+let tiesCount = 0;
+let roundsCount = 1;
 
 const getComputerChoice = () => {
     const random = Math.floor(Math.random() * 3);
+    computerChoice.textContent = `Computer Choice: ${choices[random]}`;
     return choices[random];
 };
 
-const getHumanChoice = () => {
-    let choice = 0;
-    let validInput = false;
-    while (!validInput) {
-        try {
-            choice = Number.parseInt(prompt(`
-                Rock-Paper-Scissors
-                0 -> rock
-                1 -> paper
-                2 -> scissors
-                what's your choice (0-2): `));
-            if (!Number.isInteger(choice)) {
-                throw new TypeError('Must be an integer');
-            }
-            if (choice >= 0 && choice <= 2) {
-                validInput = true
-            } else {
-                throw new TypeError("Must be between 0 and 2");
-            }
-        } catch (err) {
-            globalThis.alert(err);
-        }
-    }
-    return choices[choice];
-};
-
-const playRound = () => {
-    const computerChoice = getComputerChoice();
-    const humanChoice = getHumanChoice();
-
-    console.log(`
-        Human Choice = ${humanChoice}
-        Computer Choice = ${computerChoice}`);
-
-
-    if (computerChoice === humanChoice) {
-        return "It's a Tie";
-    }
-
+const getHumanChoice = humanChoice => {
     switch (humanChoice) {
-        case "rock":
-            computerChoice === "paper" ? computerScore++ : humanScore++;
-            return computerChoice === "paper" ? "You lose! Paper beats Rock" : "You win! Rock beats Paper";
-        case "paper":
-            computerChoice === "scissors" ? computerScore++ : humanScore++;
-            return computerChoice === "scissors" ? "You lose! Scissor beats Paper" : "You win! Paper beats Rock";
-        case "scissors":
-            computerChoice === "rock" ? computerScore++ : humanScore++;
-            return computerChoice === "rock" ? "You lose! Rock beats Scissors" : "You win! Scissors beats Paper";
+        case 'rock':
+            playerChoice.textContent = 'Player Choice: 👊';
+            return '👊';
+        case 'paper':
+            playerChoice.textContent = 'Player Choice: ✋';
+            return '✋';
+        case 'scissors':
+            playerChoice.textContent = 'Player Choice: ✌️';
+            return '✌️';
     }
 };
 
-const playGame = () => {
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1}`);
-        console.log(playRound());
+const playRound = (player, computer) => {
+    switch (player) {
+        case '👊':
+            if (computer == '✋') {
+                computerScore++;
+                result.textContent = 'Result: You lose! Paper beats Rock';
+            } else {
+                humanScore++;
+                result.textContent = 'Result: You win! Rock beats Paper';
+            }
+            break;
+        case '✋':
+            if (computer == '✌️') {
+                computerScore++;
+                result.textContent = 'Result: You lose! Scissor beats Paper';
+            } else {
+                humanScore++;
+                result.textContent = 'Result: You win! Paper beats Rock';
+            }
+            break;
+        case '✌️':
+            if (computer == '👊') {
+                computerScore++;
+                result.textContent = 'Result: You lose! Rock beats Scissors';
+            } else {
+                humanScore++;
+                result.textContent = 'Result: You win! Scissors beats Paper';
+            }
     }
-    console.log(`
-        Ties: ${5 - (humanScore + computerScore)}
-        Human Score: ${humanScore}
-        Computer Score: ${computerScore}
-        `);
+};
 
-    if (humanScore > computerScore) {
-        console.log(`Congrats, You are winner!`);
+const playGame = pChoice => {
+    if (roundsCount === 6) return;
+    roundsCount++;
+    const humanChoice = getHumanChoice(pChoice);
+    const computerChoice = getComputerChoice();
+
+    if (computerChoice == humanChoice) {
+        result.textContent = "Result: It's a Tie";
+        tiesCount++;
     } else {
-        console.log(`Sorry, You are loser!`);
+        playRound(humanChoice, computerChoice);
     }
-}
 
-playGame()
+    pScore.textContent = `Player Score: ${humanScore}`;
+    cScore.textContent = `Computer Score: ${computerScore}`;
+    ties.textContent = `Ties: ${tiesCount}`;
+    if (roundsCount === 6) {
+        if (humanScore === computerScore) {
+            winner.textContent = "It's Tied";
+            winner.classList.add('result');
+        } else if (humanScore > computerScore) {
+            winner.textContent = `Congrats, You are the winner!`;
+            winner.classList.add('pChoice');
+        } else {
+            winner.textContent = `Sorry, You are the loser!`;
+            winner.classList.add('cChoice');
+        }
+    } else {
+        roundNum.textContent = `Round: ${roundsCount}`;
+    }
+};
